2e9336bc0b11ae5124bb00fb03520ae2c980b908
vividavila98 <vdavila@ucsd.edu>
2020-07-30 00:21:42 -0700

    started on form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import '../styles/login.scss';
-import logo from '../assets/MoneyWise.png';
 
 
-function Login() {
+export default function Login() {
+
   return (
     <div className="login">
       <div className='login-box'>
-          <img src={logo} />
-          <p>New way to save money</p>
+        <h3>MoneyWise</h3>
+        <p className='sub-text'>New way to save money</p>
+        <Form name="normal_login" className="login-form">
+          <Form.Item name="email" rules={[{ type: "email", required: true, message: 'Please input your Email!' }]}>
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+          </Form.Item>
+          <Form.Item>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   );
-}
-
-export default Login;
+};
